6b3bd1521df25d93bdfcc0358cca13967b7636b1
Cocoa-wlp <136148266@qq.com>
2018-09-25 23:40:07 +0800

    first commit
    start iOS system lib study

---
+++ iOSSystemLibStudy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOSSystemLibStudy
+//
+//  Created by looperwang on 2018/9/25.
+//  Copyright © 2018年 looperwang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
