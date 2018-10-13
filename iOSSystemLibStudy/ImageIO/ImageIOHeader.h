a28039f224becf002f37498d2573dc953388f625
looperwang <136148266@qq.com>
2018-10-13 11:25:03 +0800

    resource

--- iOSSystemLibStudy/ImageIO/ImageIOHeader.h
+++ iOSSystemLibStudy/ImageIO/ImageIOHeader.h
@@ -9,18 +9,21 @@
 #ifndef ImageIOHeader_h
 #define ImageIOHeader_h
 
+/* 资料
+ http://blog.leichunfeng.com/blog/2017/02/20/talking-about-the-decompression-of-the-image-in-ios/
+ https://www.jianshu.com/p/b36b2cc4c1c0
+ https://www.jianshu.com/p/a6c673678f36
+ https://my.oschina.net/u/2340880/blog/838680
+ https://developer.apple.com/library/archive/documentation/GraphicsImaging/Conceptual/ImageIOGuide/imageio_intro/ikpg_intro.html#//apple_ref/doc/uid/TP40005462-CH201-TPXREF101
+*/
+
 /*
  ImageIO - 包含基本头文件
  ImageIOBase - 包含一系列宏
- CGImageProperties -
- CGImageMetadata -
- CGImageSource -
- CGImageDestination -
+ CGImageProperties - 定义了框架中使用的字符串常量和宏
+ CGImageMetadata - 图片文件元数据
+ CGImageSource - 负责读取图片数据
+ CGImageDestination - 负责写入图片数据
  */
-
-//https://www.jianshu.com/u/b3f97f005731
-//http://blog.leichunfeng.com/blog/2017/02/20/talking-about-the-decompression-of-the-image-in-ios/
-//https://my.oschina.net/u/2340880?tab=newest&catalogId=3294042
-//https://developer.apple.com/library/archive/documentation/GraphicsImaging/Conceptual/ImageIOGuide/imageio_intro/ikpg_intro.html#//apple_ref/doc/uid/TP40005462-CH201-TPXREF101
 
 #endif /* ImageIOHeader_h */
