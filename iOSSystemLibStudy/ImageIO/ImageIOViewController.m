c117baa92af58cb8717b2db928041127d3c416a8
looperwang <looperwang@tencent.com>
2018-10-22 22:31:34 +0800

    CGImageSourceRef learn

--- iOSSystemLibStudy/ImageIO/ImageIOViewController.m
+++ iOSSystemLibStudy/ImageIO/ImageIOViewController.m
@@ -20,11 +20,63 @@
     
     self.title = @"ImageIO";
     self.view.backgroundColor = [UIColor whiteColor];
+    
+    CFArrayRef arrayRef = CGImageSourceCopyTypeIdentifiers();
+    CFIndex count = CFArrayGetCount(arrayRef);
+    for (CFIndex index = 0; index < count; index++) {
+        const void *value = CFArrayGetValueAtIndex(arrayRef, index);
+        CFStringRef stringRef = (CFStringRef)value;
+        const char *c_str = CFStringGetCStringPtr(stringRef, kCFStringEncodingUTF8);
+        printf("%s\n", c_str);
+    }
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+
+
+
+
+
+
+
+- (NSArray<UIImage *> *)imagesWithFilePath:(NSString *)filePath
+{
+    if (filePath.length == 0)
+        return nil;
+    
+    NSURL *url = [NSURL fileURLWithPath:filePath isDirectory:NO];
+    if (!url)
+        return nil;
+    
+    CGImageSourceRef imageSourceRef = CGImageSourceCreateWithURL((CFURLRef)url, NULL);
+    if (!imageSourceRef)
+        return nil;
+    
+    NSMutableArray<UIImage *> *array = [NSMutableArray<UIImage *> new];
+    
+    size_t imageCount = CGImageSourceGetCount(imageSourceRef);
+    for (size_t index = 0; index < imageCount; index++) {
+        CGImageRef imageRef = CGImageSourceCreateImageAtIndex(imageSourceRef, index, NULL);
+        if (!imageRef)
+            continue;
+        
+        UIImage *image = [UIImage imageWithCGImage:imageRef];
+        if (!image) {
+            CGImageRelease(imageRef);
+            continue;
+        }
+        
+        [array addObject:image];
+        CGImageRelease(imageRef);
+    }
+    
+    CFRelease(imageSourceRef);
+    
+    return [NSArray<UIImage *> arrayWithArray:array];
 }
 
 @end
